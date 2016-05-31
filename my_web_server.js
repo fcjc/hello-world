7ff798518b64b9540d1abbc3b1b12095da019656
Johnny Chow <jchow@financialcontent.com>
2016-05-31 11:08:49 -0700

    Delete my_web_server.js

--- my_web_server.js
+++
@@ -1,9 +0,0 @@
-var http = require('http');
-var port = process.env.PORT || 5000;
-
-http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello World\n');
-}).listen(port, function() {
-  console.log("Listening on " + port);
-});

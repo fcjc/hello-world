49ed53203a53ef346e0abe3f74b185c194fa6d83
Johnny Chow <jchow@financialcontent.com>
2016-05-31 10:54:29 -0700

    Update my_web_server.js

--- my_web_server.js
+++ my_web_server.js
@@ -1,8 +1,9 @@
 var http = require('http');
+var port = process.env.PORT || 5000;
 
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
   res.end('Hello World\n');
-}).listen(8080);
-
-console.log('Server running on port 8080.');
+}).listen(port, function() {
+  console.log("Listening on " + port);
+});

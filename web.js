15d13337bf83f7bfe474f1914d64c1ffd8373bca
Johnny Chow <jchow@financialcontent.com>
2016-05-31 12:49:23 -0700

    Update web.js

--- web.js
+++ web.js
@@ -3,7 +3,7 @@ var port = process.env.PORT || 5000;
 
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello World\n\n Flynn on port '+port+' from container '+process.env.HOSTNAME+'\n');
+  res.end('Hello World\n');
 }).listen(port, function() {
   console.log("Listening on " + port);
 });
